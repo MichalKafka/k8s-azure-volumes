52422604d1dc1c1ab5882c06957547b8c8c675a8
Michal Kafka <36236607+MichalKafka@users.noreply.github.com>
2023-08-02 21:56:03 +0200

    Delete pvc4-fileshare-persistent directory

--- pvc4-fileshare-persistent/azure-file.sh
+++
@@ -1,8 +0,0 @@
-#az storage account create -n kafkam1storageaccount -g MC_aks1_aks1_westeurope -l westeurope --sku Standard_LRS 
-#export AZURE_STORAGE_CONNECTION_STRING=$(az storage account show-connection-string -n kafkam1storageaccount -g MC_aks1_aks1_westeurope -o tsv)
-#az storage share create -n kafkam1share --connection-string $AZURE_STORAGE_CONNECTION_STRING
-STORAGE_KEY=$(az storage account keys list --resource-group MC_aks1_aks1_westeurope --account-name kafkam1storageaccount --query "[0].value" -o tsv)
-echo Storage account key: $STORAGE_KEY
-
-kubectl create secret generic azure-secret --from-literal=azurestorageaccountname=kafkam1storageaccount --from-literal=azurestorageaccountkey=$STORAGE_KEY
-
